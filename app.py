97ec269bfefb8021096602fa3445cf7f99cd7e66
JerPaz <jeramiepaz@gmail.com>
2022-01-19 23:15:10 -0500

    very preliminary working stage, added base functionality of adding playlist and tracks to playlist, check TODOs and other comments

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import re
 from tabnanny import check
+from urllib import request
+from xmlrpc.client import Boolean
 import spotify_script
 from os import name
 from pytube import YouTube, Playlist, Search
@@ -14,7 +16,7 @@ spot_all_playlists_name_tracks_dict = spotify_script.all_playlists_name_tracks_d
 # TODO after testing, change from limit of 5 to all songs in playlist
 # TODO also after testing, change from single to all playlist (maybe?)
 def spot_playlist_tracks(spotify_playlist_name):
-    single_playlist_dict = {}
+    single_playlist_dict = {'playlist_title': spotify_playlist_name, 'items': []}
 
     index = 0
     while index < 5:
@@ -26,15 +28,16 @@ def spot_playlist_tracks(spotify_playlist_name):
         video_id = yt_video_url[yt_video_url.find('v=')+2:]
         print(video_id)
         print(yt_video_url)
-        single_playlist_dict[spotify_playlist_name] = {'song': song, 
-                                                       'artist': artist,
-                                                       'yt_video_url': yt_video_url,
-                                                       'video_id': video_id}
+        single_playlist_dict['items'].append({'song': song, 
+                                     'artist': artist,
+                                     'yt_video_url': yt_video_url,
+                                     'video_id': video_id,})
         index+=1
     
     return single_playlist_dict
 
-k_music_playlist = spot_playlist_tracks('K-Music')
+k_music_spotify_playlist = spot_playlist_tracks('K-Music')
+print(k_music_spotify_playlist)
 
 # TODO change authentication so as not to login everytime
 flow = InstalledAppFlow.from_client_secrets_file(
@@ -42,9 +45,7 @@ flow = InstalledAppFlow.from_client_secrets_file(
     scopes=["https://www.googleapis.com/auth/youtube.force-ssl"])
 flow.run_local_server(port=8080, prompt='consent')
 credentials = flow.credentials
-print(credentials.to_json())
 
-# TODO interact with Google API to put videos in playlist
 youtube = build("youtube", "v3", credentials=credentials)
 request_playlist_list = youtube.playlists().list(
     part="snippet, contentDetails", 
@@ -62,7 +63,8 @@ def get_existing_playlists(response_obj):
         playlist_total_videos = curr_yt_existing_playlist['contentDetails']['itemCount']
         playlist_details = {'id': playlist_id, 
                             'title': playlist_title,
-                            'total_videos': playlist_total_videos}
+                            'total_videos': playlist_total_videos,
+                            'spotify_playlist': None}
         exisiting_playlists[playlist_title] = playlist_details
         print(playlist_id, playlist_title, playlist_total_videos)
     
@@ -75,12 +77,70 @@ def check_playlist_exist(check_playlist, existing_playlists):
         return False
 
 existing_playlists = get_existing_playlists(response_exisiting_playlists)
-print(existing_playlists)
 print('K-Music exist: {}'.format(check_playlist_exist('K-Music', existing_playlists)))
 print('dummy_playlist: {}'.format(check_playlist_exist('dummy_playlist', existing_playlists)))
 
-# TODO check if playlist exist, if not, then create playlist and populate it
+# IT WORKS (with a huge astreik; aka as long as the playlist does not exist)
 # TODO check if playlist exist, if so, check if it has all songs; if not, then repopulate playlist with correct tracks
-# TODO will require requests to insert into playlist using API playlistitems insert function
+# TODO update exisitng playlist values once tracks are inserted into playlist
+# TODO change public/private settings to unlisted for playlists
+# TODO make the code more readable (probably separate into separate python file and have app.py as running the functions and main calls)
 
+def create_playlist(spot_playlist):
+    playlist_name = spot_playlist['playlist_title']
+    if check_playlist_exist(playlist_name, existing_playlists) == False:
+        request = youtube.playlists().insert(
+            part='snippet',
+            body={
+                'snippet': {
+                'title': playlist_name
+                }
+            }
+        )
+        response = request.execute()
+        return {'id': response['id'], 
+                'title': response['snippet']['title'],
+                'total_videos': 0, #response['contentDetails']['itemCount'],
+                'spotify_playlist': spot_playlist}
+
+k_music_yt_playlist = create_playlist(k_music_spotify_playlist)
+existing_playlists[k_music_yt_playlist['title']] = (k_music_yt_playlist)
+
+def insert_tracks(yt_playlist):
+    request_item_list = youtube.playlistItems().list(
+        part='id',
+        playlistId = yt_playlist['id']
+    )
+    response_item_list = request_item_list.execute()
+    exisiting_tracks_id_only = []
+
+    for i in range(len(response_item_list['items'])):
+        check_against = response_item_list['items'][i]['snippet']['resourceId']['videoId']
+        exisiting_tracks_id_only.append(check_against)
+
+    for i in range(len(yt_playlist['spotify_playlist']['items'])):
+        yt_playlist_id = yt_playlist['id']
+        yt_video_id = yt_playlist['spotify_playlist']['items'][i]['video_id']
+        if yt_video_id not in exisiting_tracks_id_only:
+            request = youtube.playlistItems().insert(
+                part="snippet",
+                body={
+                    "snippet": {
+                    "playlistId": yt_playlist_id,
+                    "resourceId": {
+                        "kind": "youtube#video",
+                        "videoId": yt_video_id
+                    }
+                    }
+                }
+            )
+        request.execute()
+
+insert_tracks(existing_playlists['K-Music'])
+
+def delete_playlist(yt_playlist):
+    request = youtube.playlistItems().delete(
+        id=yt_playlist['id']
+    )
+    request.execute()
 
