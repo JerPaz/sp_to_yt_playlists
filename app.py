09255aa02b676207d078f6399704039ae1b2f2ae
JerPaz <jeramiepaz@gmail.com>
2022-01-22 19:56:07 -0500

    incorporated pickle with user auth with help

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os 
 import spotify_script
 import yt_script
+import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
 def main():
-    in_single_spotify_playlist = spotify_script.spot_playlist_tracks('K-Music', 
-        spot_all_playlists_name_tracks_dict, len(spot_all_playlists_name_tracks_dict['K-Music']))
+    in_single_spotify_playlist = spotify_script.spot_playlist_tracks('K-Music Test', 
+        spot_all_playlists_name_tracks_dict, 5) #len(spot_all_playlists_name_tracks_dict['K-Music']))
 
     youtube = login_authenticate()
 
@@ -18,13 +20,41 @@ def main():
     yt_playlist = yt_script.create_playlist(in_single_spotify_playlist, yt_existing_playlists, youtube)
     yt_script.insert_tracks(yt_playlist, youtube)
 
-# TODO change authentication so as not to login every time
+# All credit to Corey Schafer and his OAuth tutorial
+# Taken straight from the snippet provided as no real need to change it
+# Reference: https://gist.github.com/CoreyMSchafer/ea5e3129b81f47c7c38eb9c2e6ddcad7
 def login_authenticate():
-    flow = InstalledAppFlow.from_client_secrets_file(
-        "client_secrets.json", 
-        scopes=["https://www.googleapis.com/auth/youtube.force-ssl"])
-    flow.run_local_server(port=8080, prompt='consent')
-    credentials = flow.credentials
+    credentials = None
+    # token.pickle stores the user's credentials from previously successful logins
+    if os.path.exists('token.pickle'):
+        print('Loading Credentials From File...')
+        with open('token.pickle', 'rb') as token:
+            credentials = pickle.load(token)
+    
+    
+    # If there are no valid credentials available, then either refresh the token or log in.
+    if not credentials or not credentials.valid:
+        if credentials and credentials.expired and credentials.refresh_token:
+            print('Refreshing Access Token...')
+            credentials.refresh(Request())
+        else:
+            print('Fetching New Tokens...')
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secrets.json',
+                scopes=[
+                    'https://www.googleapis.com/auth/youtube.force-ssl'
+                ]
+            )
+
+            flow.run_local_server(port=8080, prompt='consent',
+                                authorization_prompt_message='')
+            credentials = flow.credentials
+
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as f:
+                print('Saving Credentials for Future Use...')
+                pickle.dump(credentials, f)
+
     youtube = build("youtube", "v3", credentials=credentials)
     return youtube
 
